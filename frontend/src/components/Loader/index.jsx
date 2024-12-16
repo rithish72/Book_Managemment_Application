1b94fe67215c4517070a4bc8c9661d7613b815d1
Kurapati Rithish <120491962+rithish72@users.noreply.github.com>
2024-12-16 17:54:30 +0530

    Add files via upload

---
+++ frontend/src/components/Loader/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoaderImg from "../../images/loader.svg";
+import "./index.css";
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <img src={LoaderImg} alt="Loading, please wait" />
+    </div>
+  );
+}
+
+export default Loader;
